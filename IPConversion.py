80f155260bb0f10a55e01d55351559a1de380294
Victorydevlo <victoryukaegbu.vu@gmail.com>
2025-01-27 00:55:41 +0000

    adding binary fucntion

--- IPConversion.py
+++ IPConversion.py
@@ -7,8 +7,14 @@ def decimal(ip):
         return numberconverted
     except ValueError as error:
         return "invalid input: ", error ,"."
+    
+def binary(ip):
+    try:
+        binaryconverted = bin(int(ipaddress.IPv4Address(ip)))
+    except ValueError as errore:
+        return "invalid input: ", errore ,"."
 
 if __name__ == "__main__":
     userinput = input("Enter IP adress").strip()
     result = decimal(userinput)
-    print(result)
+    print("Number conversion", result)
