8dca2b253ef9d66e5ecb1d284e25f1657e9db2c6
Victorydevlo <victoryukaegbu.vu@gmail.com>
2025-01-27 00:51:28 +0000

    first commit

---
+++ IPConversion.py
@@ -0,0 +1,14 @@
+import ipaddress
+
+
+def decimal(ip):
+    try:
+        numberconverted = int(ipaddress.ip_address(ip))
+        return numberconverted
+    except ValueError as error:
+        return "invalid input: ", error ,"."
+
+if __name__ == "__main__":
+    userinput = input("Enter IP adress").strip()
+    result = decimal(userinput)
+    print(result)
